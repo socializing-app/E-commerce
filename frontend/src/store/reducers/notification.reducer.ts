e846ad3589e0c6e20a542671bd7f88a2f6f1ad31
peter <peter@attend2it.co.uk>
2021-07-21 00:15:28 +0100

    Creating Toaster with Notification actions.

--- frontend/src/store/reducers/notification.reducer.ts
+++ frontend/src/store/reducers/notification.reducer.ts
@@ -7,11 +7,24 @@ export const reducer = (state: State = initialState, action: ActionWithPayload):
     case notificationAction.DISPLAY: {
       return createNotification(state, action);
     }
+    case notificationAction.DISMISS: {
+      return removeNotification(state, action);
+    }
     default: return state;
   }
 }
 
 export const getNotifications = (state: State) => state.messages;
+
+const removeNotification = (state: State, action: ActionWithPayload): State => {
+  if ( !action.payload ) return state;
+
+  const updatedMessages = state.messages.filter((message: NotificationMessage) => message.id !== action.payload);
+
+  return {
+    messages: updatedMessages
+  }
+}
 
 const createNotification = (state: State, action: ActionWithPayload): State => {
   if ( action.payload.message === "" || action.payload.title === "" ) return state;
@@ -25,9 +38,21 @@ const updateNotification = (state: State, newNotification: NotificationMessage):
       ...state.messages, 
       {
         ...newNotification,
-        id: state.messages.length,
+        id: generateUniqueID(),
         timestamp: new Date().toISOString()
       }
     ]
   }
 }
+
+const generateUniqueID = (length = 30) => {
+  let code: string = '';
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  
+  for ( let i = 0; i < length; i++ ) {
+      const index = Math.floor(Math.random() * characters.length);
+      code += characters.charAt(index);
+  }
+
+  return code;
+} 
