742d164bf26d340d7020701f1006bc7ff513d1bd
peter <peter@attend2it.co.uk>
2021-07-29 17:41:29 +0100

    Creating first version of the footer.

--- frontend/src/components/footer/footer.component.tsx
+++ frontend/src/components/footer/footer.component.tsx
@@ -1,9 +1,17 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { ignoredFooter } from '../../config/ignored.components.config';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from "./footer.component.module.scss";
+import { CompanyDetails, SocialIcon, SocialIcons } from '../../config/settings.config';
+import { IconName } from '@fortawesome/fontawesome-svg-core';
 
 const FooterComponent = ( props: any ) => {
     const location = useLocation();
+    const companyName: string = CompanyDetails.name;
+    const copyright: string = CompanyDetails.copyright;
+    const socialIcons: SocialIcon[] = SocialIcons;
 
     const findIgnored = () => ignoredFooter.find((header: string) => header === location.pathname);
 
@@ -14,9 +22,20 @@ const FooterComponent = ( props: any ) => {
     return (
         <>
             { !ignored ? (
-                <>
-                    FooterComponent
-                </>
+                <div className={styles.container}>
+                    <div className={styles.company}>{ companyName }</div>
+                    <div className={styles.socials}>
+                        { socialIcons.map((icon: SocialIcon, index: number) => (
+                            <Link to={icon.link} className={styles.link} key={`social-icon-${index}`}>
+                                <FontAwesomeIcon icon={['fab', (icon.icon) as IconName]} className="mr-2" style={{ fontSize: "3rem", color: icon.colour }} />
+                            </Link>
+                        )) }
+                    </div>
+
+                    <hr/>
+
+                    <div className={styles.copyright}>{ copyright }</div>
+                </div>
             ) : null }
         </>
     ) 
