c5adfffce72ab410740e06fd3a30c3586255a395
Wo0dyStars <kerteszpetr@gmail.com>
2021-07-30 06:20:45 +0100

    Create the VIP component and the accordion. (#63)
    Co-authored-by: peter <peter@attend2it.co.uk>

--- frontend/src/components/footer/footer.component.tsx
+++ frontend/src/components/footer/footer.component.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { ignoredFooter } from '../../config/ignored.components.config';
+import NavigationComponent from './navigation/navigation.component';
+import SubscriptionComponent from './subscription/subscription.component';
 
 const FooterComponent = ( props: any ) => {
     const location = useLocation();
@@ -15,7 +17,7 @@ const FooterComponent = ( props: any ) => {
         <>
             { !ignored ? (
                 <>
-                    FooterComponent
+                    
                 </>
             ) : null }
         </>
